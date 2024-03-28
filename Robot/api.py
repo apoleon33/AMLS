95d2f75a49dd65d3b236102e3e6dfad1ffd50671
apoleon33 <simondumesny@gmail.com>
2024-03-28 12:01:08 +0100

    skeleton of the Api class

---
+++ Robot/api.py
@@ -0,0 +1,19 @@
+class Api():
+    __url: str
+
+    def __init__(self, url: str):
+        self.__url = url
+
+    @property
+    def Url(self) -> str:
+        return self.__url
+
+    @Url.setter
+    def Url(self, url: str):
+        self.__url = url
+
+    def send(self, subdomain: str, data: Any):
+        pass
+
+    def receive(self, subdomain: str) -> str:
+        pass
