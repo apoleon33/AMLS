b5b2596dbd5cdfd7d70e3be99f9d98434f642246
apoleon33 <simondumesny@gmail.com>
2024-03-28 12:11:47 +0100

    robot package the exact same as the uml

--- Robot/robot.py
+++ Robot/robot.py
@@ -1,11 +1,17 @@
+from Robot.api import Api
 from Robot.coordinate import Coordinate
 
 
 class Robot():
     __coordinates: Coordinate
+    __api: Api
 
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, api: Api):
         self.__coordinates = Coordinate(x, y)
+        self.__api = api
 
     def getCoordinate(self) -> Coordinate:
         return self.__coordinates
+
+    def updateCoordinate(self):
+        pass
