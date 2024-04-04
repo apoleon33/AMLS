4472d81fd00c90f61cb3077ea360107e88884471
apoleon33 <simondumesny@gmail.com>
2024-04-04 15:50:34 +0200

    boosted `updateCoordinate`

--- Robot/robot.py
+++ Robot/robot.py
@@ -15,12 +15,16 @@ class Robot:
         print(self.__coordinates)
         return self.__coordinates
 
-
     def updateCoordinate(self):
-        print(self.__api.receive("")['y'])
+        newCoordinate = self.__api.receive("")
+
+        if newCoordinate["success"]:  # on a bien reçu des coordonnées
+            self.addCoordinate(newCoordinate["x"], newCoordinate["y"])
+
+    def addCoordinate(self, x: int, y: int):
+        self.__coordinates.append(Coordinate(x, y))
         coordinates = self.__api.receive("")
         self.addCoordinate(coordinates['x'], coordinates['y'])
 
     def addCoordinate(self, x: int, y: int):
         self.__coordinates.append(Coordinate(x, y))
-
